1fc4d78ff231a5503ce8f9d2bb6e41dd08f22dcf
LMalfoy <kevin.sicking@gmail.com>
2023-01-27 11:00:16 +0100

    Update particle identity checking script and test files.

--- ParticleIdentities/ParticleIdentities.py
+++ ParticleIdentities/ParticleIdentities.py
@@ -14,7 +14,11 @@
 '''
 
 import pandas as pd
+import numpy as np
+import glob
 from datetime import date
+# regular dict functions the same way (being ordered) but backwards compatibility is an issue
+from collections import OrderedDict
 
 '''
 USAGE TO BE WRITTEN LELMAO
@@ -103,26 +107,58 @@ class Star:
 
 
 if __name__ == '__main__':
-    # Read particle star file 1 and star file 2
-    filename1 = '1st_selection_particles.star'
-    filename2 = '2nd_selection_particles.star'
-    first = Star(filename=filename1)
-    second = Star(filename=filename2)
 
-    # Get particle IDs from both star files using rlnImageName column
-    first_particle_names = first.dataframes[1]["rlnImageName #8"]
-    len_first = len(first_particle_names)
-    second_particle_names = second.dataframes[1]["rlnImageName #8"]
-    len_second = len(second_particle_names)
+    # Getting all particle IDs
+    particle_groups = OrderedDict()
+    for file in sorted(glob.glob("*.star")):
+        print("Found " + file + ".")
+        parser = Star(filename=file)
+        particle_series = parser.dataframes[1]["rlnImageName #8"]
+        # Saving particle ID list as well as length as an entry in particle groups dict
+        particle_groups[file] = particle_series
 
-    # Calculate the intersection, i.e. particles IDs that are found in both columns
-    intersection = pd.Series(list(set(first_particle_names) & set(second_particle_names)))
-    len_intersect = len(intersection)
+    # Analyze particle identities between groups
+    # Save identities (total & percentage) in array
+    colnames = list(particle_groups.keys())
+    num_matrix = np.ndarray([len(colnames), len(colnames)])
+    perc_matrix = np.ndarray([len(colnames), len(colnames)])
+    numResults = pd.DataFrame(data=num_matrix)
+    percResults = pd.DataFrame(data=perc_matrix)
+    # Calculate particle overlap between all groups and save in matrix
+    for i in range(len(colnames)):
+        total_num = len(particle_groups[colnames[i]])
+        for j in range(len(colnames)):
+            # Calculate overlap between two groups (first and second)
+            first = particle_groups[colnames[i]]
+            second = particle_groups[colnames[j]]
+            result = pd.Series(np.intersect1d(first, second))
+            # Save result in matrix
+            numResults.loc[i, j] = len(result)
+            percResults.loc[i, j] = len(result) / total_num
+    # Rename columns and rows (row is index)
+    numResults.columns = colnames
+    numResults.index = colnames
+    percResults.columns = colnames
+    percResults.index = colnames
+    print(numResults)
+    print(percResults)
+
+    # Calculate total overlap (anything else to compare?)
+    all_series = []
+    for series in colnames:
+        all_series.append(set(particle_groups[series]))
+    sets = map(set, all_series)
+    total_overlap = len(set.intersection(*sets))
+    print(total_overlap)
 
     # Print results and save in file
     today = date.today()
     date_string = today.strftime("%y%m%d")
-
+    '''
+    # Calculate the intersection, i.e. particles IDs that are found in both columns
+    intersection = pd.Series(list(set(first_particle_names) & set(second_particle_names)))
+    len_intersect = len(intersection)
+    
     outfile = date_string + '_particle_identity_check.txt'
     report_string = "Created on " + date_string + "\n"
     report_string += "Filename 1: " + filename1 + "\n"
@@ -134,3 +170,4 @@ if __name__ == '__main__':
                      "% ; File 2: " + str(len_intersect / len_second * 100) +"%\n"
     with open(outfile, "w") as fout:
         fout.write(report_string)
+    '''
